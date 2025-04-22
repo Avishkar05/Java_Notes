db8fa89c21aa3e60dc3e7e5a49be93567fbfc2c3
Avishkar05 <avishkarzende505@gmail.com>
2025-04-22 11:27:10 +0530

    Create Treemap_basics_ex2.java

---
+++ Collection_List/TreeMap/Treemap_basics_ex2.java
@@ -0,0 +1,28 @@
+package TreeMap;
+
+import java.util.TreeMap;
+
+public class Treemap_basics_ex2 {
+
+	public static void main(String[] args) {
+		TreeMap<Object,Object>  map = new TreeMap<Object,Object>();
+		
+		map.put(20, "Ketan");
+		map.put(10, "Avishkar");
+		map.put(70, "onkar");
+		map.put(90, "vilas");
+		map.put(200, "Sahil");
+		
+		System.out.println(map);
+		
+		//Use to print equal or greater 
+		System.out.println(map.ceilingEntry(80));
+		
+		//Use to print equal or lower 
+		System.out.println(map.floorEntry(60));
+		
+		
+		
+	}
+
+}
