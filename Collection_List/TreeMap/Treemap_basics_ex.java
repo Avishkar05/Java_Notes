85090f0561e6e988911984c9cd3d9be5a03bc8de
Avishkar05 <avishkarzende505@gmail.com>
2025-04-22 11:37:35 +0530

    Update Treemap_basics_ex.java

--- Collection_List/TreeMap/Treemap_basics_ex.java
+++ Collection_List/TreeMap/Treemap_basics_ex.java
@@ -8,12 +8,12 @@ public class Treemap_basics_ex {
 		
 		TreeMap <Object,Object> map2 = new TreeMap<Object,Object>();
 		
-		map2.put(01, "Avishkar");
-		map2.put(05, "Onkar");
-		map2.put(03, "Ketan");
-		map2.put(02, "Vilas");
-		map2.put(07, "Sahil");
-		map2.put(04, "Rohan");
+		map2.put(10, "Avishkar");
+		map2.put(20, "Onkar");
+		map2.put(30, "Ketan");
+		map2.put(50, "Vilas");
+		map2.put(70, "Sahil");
+		map2.put(40, "Rohan");
 		
 		System.out.println(map2);
 		
@@ -25,7 +25,19 @@ public class Treemap_basics_ex {
 		System.out.println(map2.pollLastEntry());
 		System.out.println(map2);
 		
-		System.out.println(map2.size());
+//		Return Higher Key then  Key
+		System.out.println(map2.higherEntry(30));
+
+//		Return Higher Key then  Parameter Key
+		System.out.println(map2.higherKey(20));
+
+//		Return Lower Key then Key
+		System.out.println(map2.lowerEntry(30));
+		
+//		Return Lower Key then  Parameter Key
+		System.out.println(map2.lowerKey(20));
+		
+		
 		
 	}
 
